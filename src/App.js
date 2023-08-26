c506649735f7d84cdae6badaf67c31e000ec45c4
Bego Alibasic <bego.alibasic@gmail.com>
2023-08-26 17:38:00 +0200

    initial comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { HeatGraph } from "./heatgraph/HeatGraph";
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => setValue(Math.floor(Math.random() * 100) + 1), 2000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"App"}>
+      <h5>Hello HeatGraph</h5>
+      <div style={{ width: "250px" }}>
+        <HeatGraph valuePercentage={100} />
+        <br />
+        <HeatGraph valuePercentage={75} />
+        <br />
+        <HeatGraph valuePercentage={50} />
+        <br />
+        <HeatGraph valuePercentage={25} />
+        <br />
+        <HeatGraph valuePercentage={0} />
+        <br />
+      </div>
+      <br />
+      <h5>With Animation</h5>
+      <div style={{ width: "250px" }}>
+        <HeatGraph valuePercentage={value} />
+      </div>
     </div>
   );
 }
